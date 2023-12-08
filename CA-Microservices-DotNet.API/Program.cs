fc0ec50e8180f10cf4bfd4004bcf65b30f478086
Alexis Juarez Nava <Alexis.Juarez.Nava.-ND@disney.com>
2023-12-08 10:49:47 -0600

    Add Identity Framework authentication and docker compose

--- CA-Microservices-DotNet.API/Program.cs
+++ CA-Microservices-DotNet.API/Program.cs
@@ -1,7 +1,11 @@
 using CA_Microservices_DotNet.Application.Services;
 using CA_Microservices_DotNet.Domain.Interfaces.Repositories;
 using CA_Microservices_DotNet.Domain.Interfaces.Services;
+using CA_Microservices_DotNet.Infrastructure;
 using CA_Microservices_DotNet.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,12 +15,38 @@ builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+//Add DBContext where the Identity tables are going to be created.
+var test = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//Add Swagger bearer token authentication 
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+
+});
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<MyDbContext>();
 
 var app = builder.Build();
+
+//NET 8 mapping of Identity tables
+app.MapIdentityApi<IdentityUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,6 +54,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
---
+++ CA-Microservices-DotNet.Infrastructure/MyDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CA_Microservices_DotNet.Infrastructure
+{
+    public class MyDbContext(DbContextOptions<MyDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
